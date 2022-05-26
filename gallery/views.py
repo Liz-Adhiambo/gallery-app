55f643509fbae638c5b98fd840b3042f8b29cbe0
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-05-26 22:41:23 +0300

    Added Bootstrap

--- gallery/views.py
+++ gallery/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'landing.html')
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'about.html')
