0417c01b3d101edc4902ef81092e44f9146b1aa3
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-05-27 21:44:50 +0300

    Added class Category and location in the models file to complete the details of an image

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Image
+from .models import Location, Category, Image
 
 
 admin.site.register(Image)
--- gallery/models.py
+++ gallery/models.py
@@ -1,11 +1,110 @@
 from django.db import models
 from django.forms import ImageField
+from django.template.defaultfilters import slugify
+from django_resized import ResizedImageField
+from django.utils import timezone
+from uuid import uuid4
+from django.urls import reverse
+
+class Location(models.Model):
+    name = models.CharField(max_length =50)
+
+    @classmethod
+    def tag_articles(cls):
+        tags = cls.objects.all()
+        return tags
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_location(self, update):
+        self.name = update
+        self.save()
+
+    @classmethod
+    def get_location_id(cls, id):
+        locate = Location.objects.get(pk = id)
+        return locate
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    title = models.CharField(null=True, blank=True, max_length=200)
+
+    #Utility Variable
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.title, self.uniqueId)
+
+
+    def get_absolute_url(self):
+        return reverse('category-detail', kwargs={'slug': self.slug})
+
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+
+
+        self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+        super(Category, self).save(*args, **kwargs)
+
+
 
 
 class Image(models.Model):
     name = models.CharField(max_length = 60)
     photo = models.ImageField(upload_to = 'uploads/')
     description = models.TextField()
+    altText = models.TextField(null=True, blank=True)
+    hashtags = models.CharField(null=True, blank=True, max_length=300)
+
+    ##ImageFields
+    squareImage = ResizedImageField(size=[1000, 1000], crop=['middle', 'center'], default='default_square.jpg', upload_to='square')
+    landImage = ResizedImageField(size=[2878, 1618], crop=['middle', 'center'], default='default_land.jpg', upload_to='landscape')
+    tallImage = ResizedImageField(size=[1618, 2878], crop=['middle', 'center'], default='default_tall.jpg', upload_to='tall')
+
+    ##Related Fields
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.CASCADE)
+    location=models.ForeignKey(Location, null=True, blank=True, on_delete=models.CASCADE)
+
+    #Utility Variable
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.category.title, self.uniqueId)
+
+
+    def get_absolute_url(self):
+        return reverse('image-detail', kwargs={'slug': self.slug})
+
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {}'.format(self.category.title, self.uniqueId))
+
+
+        self.slug = slugify('{} {}'.format(self.category.title, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+        super(Image, self).save(*args, **kwargs)
     
 
     def save_image(self):
