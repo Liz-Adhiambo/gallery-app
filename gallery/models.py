41ade72dae4717fcebb0a236f1d88397aa568858
Elizabeth Adhiambo <adhiamboliz3@gmail.com>
2022-05-27 15:18:45 +0300

    Added Image model and made first migration

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.forms import ImageField
 
+
+class Image(models.Model):
+    name = models.CharField(max_length = 60)
+    photo = models.ImageField(upload_to = 'uploads/')
+    description = models.TextField()
+    
 # Create your models here.
